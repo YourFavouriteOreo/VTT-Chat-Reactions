6173a51573eecc3f818fbff583481f4886c85ad0
Liu Jiawei <88jliu88@gmail.com>
2023-01-11 21:01:39 -0500

    fix: render keycap emojis

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,6 @@
 export function toCodePoints(unicodeSurrogates) {
   const result = [...unicodeSurrogates].map((c) => c.codePointAt(0).toString(16));
-  if (result.length === 2 && result[1] === "fe0f")
-    result.pop();
+  if (result[1] === "fe0f" && result.length <= 3)
+    result.splice(1, 1);
   return result;
 }
