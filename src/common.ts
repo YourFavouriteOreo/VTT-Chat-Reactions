48407da4b78a4eba7d529bd48138a3d62dd82bb9
Liu Jiawei <88jliu88@gmail.com>
2023-01-11 20:44:39 -0500

    fix: render emojis that need fe0f codepoint

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,6 @@
 export function toCodePoints(unicodeSurrogates) {
-  return [...unicodeSurrogates].map((c) => c.codePointAt(0).toString(16)).filter((c) => c !== 'fe0f');
+  const result = [...unicodeSurrogates].map((c) => c.codePointAt(0).toString(16));
+  if (result.length === 2 && result[1] === "fe0f")
+    result.pop();
+  return result;
 }
